b87101f00157700649a72a72bebe292ce62b7ef6
MasterWolf2077 <damonmiche8@gmail.com>
2025-08-26 10:40:28 +0200

    commit

---
+++ Lab/break_statement-Stuck_in_loop.py
@@ -0,0 +1,6 @@
+while True:
+    word = input("Your're stuck in a infinite loop!\n Enter a secret word to break the loop.")
+    if word == "chupacabra":
+        break
+    
+print("You've succeessfully left the loop!")
